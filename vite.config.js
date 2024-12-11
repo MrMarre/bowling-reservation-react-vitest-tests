0561152994bf37956f7439b389c810330224839a
Martin <martin.kolberg@outlook.com>
2024-12-11 14:01:11 +0100

    Hopefully final

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react({ jsxRuntime: 'automatic' })],
   test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup-tests.js',
+    css: false,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'lcov', 'json-summary'],
       include: ['src/**/*.js', 'src/**/*.jsx'],
       exclude: ['./src/main.jsx', './src/App.jsx', './src/router.jsx'],
     },
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setup-tests.js',
-    css: false,
   },
 });
